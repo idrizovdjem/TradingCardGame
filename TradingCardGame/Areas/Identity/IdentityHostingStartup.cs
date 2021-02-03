73a6496643a23f4926bb8dbe359550dfe4243e65
idrizovdjem <idrizovdjem@gmail.com>
2021-02-03 15:33:29 +0200

    Change Login and Register pages design

---
+++ TradingCardGame/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TradingCardGame.Data;
+using TradingCardGame.Data.Models;
+
+[assembly: HostingStartup(typeof(TradingCardGame.Areas.Identity.IdentityHostingStartup))]
+namespace TradingCardGame.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
