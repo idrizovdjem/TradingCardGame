94b09776c09f95ac14d7fcb28311e38dc3ae0113
idrizovdjem <idrizovdjem@gmail.com>
2021-02-03 15:39:27 +0200

    Add channel Index view.

--- TradingCardGame/Controllers/ChannelController.cs
+++ TradingCardGame/Controllers/ChannelController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 namespace TradingCardGame.Controllers
 {
+    [Authorize]
+    [AutoValidateAntiforgeryToken]
     public class ChannelController : Controller
     {
         public IActionResult Index()
--- TradingCardGame/Controllers/HomeController.cs
+++ TradingCardGame/Controllers/HomeController.cs
@@ -1,31 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using TradingCardGame.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TradingCardGame.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
+            return Redirect("/Channel/Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
